86df72add7b97bc152886b1445fc79f788e2563d
EVGA2048 <xxAp2005@outlook.com>
2025-03-26 22:54:59 +0800

    Update:增加了欢迎面板和自动面板尺寸缩进

--- cmdline_menu.py
+++ cmdline_menu.py
@@ -1,10 +1,31 @@
+#Developed by xxAp2005
+
 #import time
+# 导入datetime模块
+import datetime
+
+import os
 
 
+#检测是否执行初始化
+initialized = "false"
+
+#获取系统用户名
+username = os.getlogin()
+
+#初始化头空格全局变量
+headerSpace = "    "
+
+# 获取当前日期
+current_date = datetime.date.today()
+
+# 格式化日期
+formatted_date = current_date.strftime("%Y-%m-%d")
 
 
-
+#从目标程序获取菜单类型
 def initialize_menu_type(menu_type):
+    global initialized
     global menuType
     if menu_type not in ["small", "medium", "large"]:
         print("无效的菜单类型. 选择 'small', 'medium', 或者 'large'.")
@@ -12,56 +33,70 @@ def initialize_menu_type(menu_type):
         menuType = "small"
     print("debug: menuType设置为" + menu_type)
     menuType = menu_type
+    initialized = "true"
+    print("debug:initialized = " + initialized + " menuType=" + menuType)
 
 
-
-
-
-
-
-
-def clear_cmdline_x10():                                    #调用此函数生成10行空格用于清屏
+def clear_cmdline_x10():                                    #生成10行空格用于清屏
     for _ in range(10):
         print(" ")
 
-def short_border():
+def small_border():                                         #打印小尺寸边框
     print("+-----+-----+-----+-----+-----+-----+")
 
-def medium_border():
+def medium_border():                                        #打印中尺寸边框
     print("+-----+-----+-----+-----+-----+-----+-----+-----+")
 
-def large_border():
+def large_border():                                         #打印大尺寸边框
     print("+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+")
 
-def drawBorder():
-    if menuType == "short":
-        short_border()
+def drawBorder(menuType):                                           #打印边框
+    if menuType == "small":
+        small_border()
     
     if menuType == "medium":
         medium_border()
     
     if menuType == "large":
         large_border()
+
+def header_space(menuType):
+    global headerSpace
+    if menuType == "small":
+        headerSpace = "    "
     
-def create_option(sequence_number, option_text):
-    if menuType == "short":
+    if menuType == "medium":
+        headerSpace = "        "
+
+    if menuType == "large":
+        headerSpace = "            "
+    
+def create_option(sequence_number, option_text):            #新建选项
+    global menuType
+    if menuType == "small":
         print("    ")
-        print("["+ sequence_number +"]" + option_text)
+        print(headerSpace + "["+ sequence_number +"]" + option_text)
 
     if menuType == "medium":
         print("            ")
-        print("            ["+ sequence_number +"]" + option_text)
+        print(headerSpace + "            ["+ sequence_number +"]" + option_text)
 
     if menuType == "large":
         print("            ")
-        print("            ["+ sequence_number +"]" + option_text)
+        print(headerSpace + "            ["+ sequence_number +"]" + option_text)
 
-def read_selection():
+def read_selection():                                       #读取选项
     selection = int(input("请输入选项序号："))
     return selection
 
-def singlespace():
+def singlespace():                                          #换行
     print(" ")
 
-def raw_text(text):
+def raw_text(text):                                         #打印文本
     print(text)
+
+def welcome_panel(motd):
+    singlespace()
+    print(headerSpace + "欢迎!  " + username + "     现在是 " + formatted_date)
+    singlespace()
+    raw_text(headerSpace + motd)
--- example_main.py
+++ example_main.py
@@ -4,20 +4,109 @@ import cmdline_menu
 
 
 
+menuType = "small"
+cmdline_menu.initialize_menu_type("small")
 
-menuType = "medium"
-cmdline_menu.initialize_menu_type(menuType)
+
+
 cmdline_menu.clear_cmdline_x10()
-cmdline_menu.drawBorder()
+cmdline_menu.drawBorder("small")
 cmdline_menu.raw_text("            菜单标题")
+cmdline_menu.singlespace()
+cmdline_menu.raw_text("输入任意选项继续...")
 cmdline_menu.create_option("1","选项1")
 cmdline_menu.create_option("2","选项2")
 cmdline_menu.singlespace()
-cmdline_menu.drawBorder()
+cmdline_menu.drawBorder("small")
 option = cmdline_menu.read_selection()
+
+
+
+def main_menu():
+    global option
+    global menuType
+    cmdline_menu.drawBorder(menuType)
+    cmdline_menu.welcome_panel("今天想做点什么呢？")
+    cmdline_menu.create_option("1","打印Hello World")
+    cmdline_menu.create_option("2","更改菜单尺寸")
+    cmdline_menu.create_option("3","显示菜单尺寸")
+    cmdline_menu.singlespace()
+    cmdline_menu.drawBorder(menuType)
+
+    option = cmdline_menu.read_selection()
+
+    match option:
+        case 1:
+            global pause_key
+            cmdline_menu.clear_cmdline_x10()
+            cmdline_menu.drawBorder(menuType)
+            print("Hello World")
+            cmdline_menu.drawBorder(menuType)
+            cmdline_menu.raw_text("请按任意键继续")
+            pause_key = input()
+            main_menu()
+        case 2:
+            change_menu_type()
+        case 3:
+            print("debug: menuType=" + menuType)
+            main_menu()
+
+def change_menu_type():
+    global option
+    global menuType
+    cmdline_menu.clear_cmdline_x10()
+    cmdline_menu.drawBorder(menuType)
+    cmdline_menu.singlespace()
+    cmdline_menu.raw_text("当前菜单尺寸为" + menuType)
+    cmdline_menu.singlespace()
+    cmdline_menu.raw_text("是否进行更改？")
+    cmdline_menu.create_option("1","更改为small")
+    cmdline_menu.create_option("2","更改为medium")
+    cmdline_menu.create_option("3","更改为large")
+    cmdline_menu.create_option("0","不更改")
+    cmdline_menu.singlespace()
+    cmdline_menu.drawBorder(menuType)
+
+    option = cmdline_menu.read_selection()
+
+    match option:
+        case 1:
+            menuType = "small"
+            cmdline_menu.drawBorder(menuType)
+            cmdline_menu.raw_text("已更改菜单尺寸为" + menuType)
+            cmdline_menu.singlespace()
+            cmdline_menu.raw_text("按任意键返回")
+            cmdline_menu.drawBorder(menuType)
+            menuType = "small"
+        case 2:
+            menuType = "medium"
+            cmdline_menu.drawBorder(menuType)
+            cmdline_menu.raw_text("已更改菜单尺寸为" + menuType)
+            cmdline_menu.singlespace()
+            cmdline_menu.raw_text("按任意键返回")
+            cmdline_menu.drawBorder(menuType)
+            menuType = "medium"
+        case 3:
+            menuType = "large"
+            cmdline_menu.drawBorder(menuType)
+            cmdline_menu.raw_text("已更改菜单尺寸为" + menuType)
+            cmdline_menu.singlespace()
+            cmdline_menu.raw_text("按任意键返回")
+            cmdline_menu.drawBorder(menuType)
+            menuType = "large"
+        case 0:
+            cmdline_menu.drawBorder(menuType)
+            cmdline_menu.raw_text("未作出任何修改")
+            cmdline_menu.singlespace()
+            cmdline_menu.raw_text("按任意键返回")
+            cmdline_menu.drawBorder(menuType)
+    
+    main_menu()
 
 match option:
     case 1:
-        print("选择的结果为选项1")
+        main_menu()
     case 2:
-        print("选择的结果为选项2")
+        main_menu()
+            
+
